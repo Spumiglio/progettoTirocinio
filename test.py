3981a07d119d384152d71a3c2814716129ac6f5f
luca <luca.gugole99@gmail.com>
2021-04-23 11:19:07 +0200

    aggiunto somma vendite al dataset

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 def main():
     dativendita = pd.read_csv("students_dataset_attr.csv")
-
+    somma_vendite = dativendita["0"] + dativendita["1"] + dativendita["2"] + dativendita["3"] + \
+                    dativendita["4"] + dativendita["5"] + dativendita["6"] + dativendita["7"] + \
+                    dativendita["8"] + dativendita["9"] + dativendita["10"] + dativendita["11"]
+    dativendita.insert(1, "somma_vendite", somma_vendite, allow_duplicates=True)
     print("ciao")
 
 
