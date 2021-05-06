e4202707c5451fd04f4ada315830bc36e168349c
Spumiglio <mattiaburati@live.it>
2021-05-06 11:13:55 +0200

    Train, Test Error Fix

--- plotter.py
+++ plotter.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 # type_of_plot puo' essere H = istogramma, B = barre, L = lineare
-def plot_dataframe(df, type_of_plot="L", plot_name="Vendite totali", forecasting_indexes=0):
+def plot_dataframe(df, test, type_of_plot="L", plot_name="Vendite totali", forecasting_indexes=0):
     if type_of_plot == "L":
         df_temp = df.copy()
         x = df_temp.index
@@ -13,8 +13,10 @@ def plot_dataframe(df, type_of_plot="L", plot_name="Vendite totali", forecasting
         y = df_temp['vendite']
         f = df_temp['forecasting']
         plt.plot(x, y, 'r',  f, 'b')
+        plt.plot(test)
         plt.xlabel("Settimane")
         plt.ylabel("Totale vendite")
+        plt.legend(["Train","Forecast","Test"])
         plt.title(plot_name)
         plt.show()
 
--- test.py
+++ test.py
@@ -13,12 +13,8 @@ def main():
     df_col = weeksdistrubution(dflist[0])
 
     dativendita_colore_test = weeksdistrubution(test)
-    testlist = dataframelist(test)
-    test_col = weeksdistrubution(testlist[0])
-
-
     forecast_index = dativendita_colore.index.size - 1
-    plot_dataframe(test_col, plot_name="Test")
+    # plot_dataframe(test_col, plot_name="Test")
 
     for df in dflist:
         df_col = weeksdistrubution(df)
@@ -31,7 +27,7 @@ def main():
         forecast_date, forecast_value = average_forecasting(dativendita_colore['vendite'], week_to_forecast)
         dativendita_colore.loc[forecast_date] = forecast_value
     # plot_dataframe(dativendita_colore)
-    plot_dataframe(dativendita_colore, plot_name="Average", forecasting_indexes=forecast_index)
+    plot_dataframe(dativendita_colore,dativendita_colore_test, plot_name="Average", forecasting_indexes=forecast_index)
 
     dativendita_colore = weeksdistrubution(train)
     # testing seasonal naive
@@ -40,7 +36,7 @@ def main():
         forecast_date, forecast_value = seasonal_naive_forecasting(dativendita_colore['vendite'], week_to_forecast, 25,1)
         dativendita_colore.loc[forecast_date] = forecast_value
     # plot_dataframe(dativendita_colore)
-    plot_dataframe(dativendita_colore, plot_name="Seasonal Naive", forecasting_indexes=forecast_index)
+    plot_dataframe(dativendita_colore, dativendita_colore_test, plot_name="Seasonal Naive", forecasting_indexes=forecast_index)
 
     #  Naive
     dativendita_colore = weeksdistrubution(train)
@@ -48,23 +44,23 @@ def main():
         forecast_date, forecast_value = naive(dativendita_colore, dativendita_colore.index[dativendita_colore.index.size - 1])
         dativendita_colore.loc[forecast_date] = forecast_value
     # plot_dataframe(df, plot_name="Naive")
-    plot_dataframe(dativendita_colore, plot_name="Naive", forecasting_indexes=forecast_index)
+    plot_dataframe(dativendita_colore,dativendita_colore_test, plot_name="Naive", forecasting_indexes=forecast_index)
 
     # testing drift
     dativendita_colore = weeksdistrubution(train)
     for i in range(0, 27):
         newdf = driftmethod(dativendita_colore)
-    plot_dataframe(newdf, plot_name="Drift", forecasting_indexes=forecast_index)
+    plot_dataframe(newdf, dativendita_colore_test, plot_name="Drift", forecasting_indexes=forecast_index)
 
     dativendita_colore = weeksdistrubution(train)
     # testing seasonalexpsmooth
     modelo = seasonalExp_smoothing(dativendita_colore,27)
-    plot_dataframe(modelo, plot_name="HoltWinter", forecasting_indexes=forecast_index)
+    plot_dataframe(modelo,dativendita_colore_test, plot_name="HoltWinter", forecasting_indexes=forecast_index)
 
     dativendita_colore = weeksdistrubution(train)
     # testing simpleExpSmothing
     smpExpSmoth(dativendita_colore, 27)
-    plot_dataframe(dativendita_colore, plot_name='simpleExpSmothing', forecasting_indexes=forecast_index)
+    plot_dataframe(dativendita_colore, dativendita_colore_test, plot_name='simpleExpSmothing', forecasting_indexes=forecast_index)
 
 
 if __name__ == '__main__':
