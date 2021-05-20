40bbf2ffb3ec1b5a3d7d282a6687aefb83a020f6
Enrico <enricovrz2811@gmail.com>
2021-05-20 11:21:29 +0200

    test

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+
+from statsmodels.formula.api import ols
+from statsmodels.stats.outliers_influence import OLSInfluence
+
 from forecasting import *
 from plotter import *
 from evaluation import *
@@ -7,6 +11,7 @@ from matplotlib import pyplot
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.seasonal import STL
 from scipy import stats
+from dataPreparation import *
 
 def main():
     dativendita = pd.read_csv("students_dataset_attr.csv").sort_values(by=["giorno_uscita"])
@@ -14,7 +19,7 @@ def main():
     dativendita = best20color(dativendita)
     datetoweek(dativendita)
 
-    dativendita = filter_by_color(dativendita, 'nero')
+    dativendita = filter_by_color(dativendita, 'giallo')
     dativendita_colore = weeksdistrubution(dativendita)
 
     dativendita_colore = fill_missing_data(dativendita_colore, start=dativendita_colore.index[0],
@@ -29,8 +34,8 @@ def main():
     autocorrelazione = train.squeeze().autocorr()
 
     # Autocorrelazione con Statsmodel
-    plot_acf(train, title="autocorrelazione train", lags=None)
-    pyplot.show()
+    # plot_acf(train, title="autocorrelazione train", lags=None)
+    # pyplot.show()
 
     last_week = train.index[train.index.size - 1]
 
@@ -38,30 +43,31 @@ def main():
     train = train[(np.abs(stats.zscore(train)) < 3).all(axis=1)]
     test = test[(np.abs(stats.zscore(test)) < 3).all(axis=1)]
 
+
     # Average
     df_avg = average_forecasting(train.copy(), last_week, week_to_forecast=len(test.index))
-    plot_dataframe(df_avg, test, plot_name="Average", forecasting_indexes=forecast_index)
+    # plot_dataframe(df_avg, test, plot_name="Average", forecasting_indexes=forecast_index)
 
 
     # Seasonal Naive
     df_sn = seasonal_naive_forecasting(train.copy(), last_week, 26, 1, week_to_forecast=len(test.index))
-    plot_dataframe(df_sn, test, plot_name="Seasonal Naive", forecasting_indexes=forecast_index)
+    # plot_dataframe(df_sn, test, plot_name="Seasonal Naive", forecasting_indexes=forecast_index)
 
     #  Naive
     df_n = naive(train.copy(), last_week, week_to_forecast=len(test.index))
-    plot_dataframe(df_n, test, plot_name="Naive", forecasting_indexes=forecast_index)
+    # plot_dataframe(df_n, test, plot_name="Naive", forecasting_indexes=forecast_index)
 
     # Drift
     df_d = driftmethod(train.copy(), last_week, week_to_forecast=len(test.index))
-    plot_dataframe(df_d, test, plot_name="Drift", forecasting_indexes=forecast_index)
+    # plot_dataframe(df_d, test, plot_name="Drift", forecasting_indexes=forecast_index)
 
     # Seasonal Exp Smoothing
-    df_hw = seasonalExp_smoothing(train.copy(), len(test.index), decompositon=False)
+    df_hw = seasonalExp_smoothing(train.copy(), len(test.index), decompositon=False, box_cox=True)
     plot_dataframe(df_hw, test, plot_name="Holt-Winters", forecasting_indexes=forecast_index)
 
     # Simple Exp Smoothing
     df_ses = smpExpSmoth(train.copy(), len(test.index))
-    plot_dataframe(df_ses, test, plot_name='Simple Exponential Smoothing', forecasting_indexes=forecast_index)
+    # plot_dataframe(df_ses, test, plot_name='Simple Exponential Smoothing', forecasting_indexes=forecast_index)
 
     # Sarima
     # test score model
@@ -73,7 +79,7 @@ def main():
         print(cfg, error)
 
     cfg = ast.literal_eval(cfg)
-    df_sar = sarima_forecast(train.copy(), cfg, len(test.index), decomposition=False)
+    df_sar = sarima_forecast(train.copy(), cfg, len(test.index), decomposition=False, box_cox=True)
     plot_dataframe(df_sar, test, plot_name='Arima', forecasting_indexes=forecast_index)
 
     print('Best method: ' + evaluate_simple_forecasts(train, test, 'vendite', cfg))
--- test_forecast.py
+++ test_forecast.py
@@ -1,13 +1,48 @@
+import numpy as np
 import pandas as pd
+from scipy.stats import stats
 
-from dataPreparation import sommavendite, best20color, datetoweek, filter_by_color, weeksdistrubution
+from dataPreparation import sommavendite, best20color, datetoweek, filter_by_color, weeksdistrubution, data_splitter
+from forecasting import *
+from plotter import plot_dataframe
 
 
 def main():
-    colore = 'fantasia'
-
+    colore = 'giallo'
     df = prepare_data(colore)
+    train, test = data_splitter(df, int(len(df.index) * 0.2))
+    train = train[(np.abs(stats.zscore(train)) < 3).all(axis=1)]
+    test = test[(np.abs(stats.zscore(test)) < 3).all(axis=1)]
+    last_week = train.index[train.index.size - 1]
+    forecast_index = train.index.size - 1
 
+
+
+
+    if colore== "giallo":
+        # Average
+        df_avg = average_forecasting(train.copy(), last_week, week_to_forecast=len(test.index))
+        # plot_dataframe(df_avg, test, plot_name="Average", forecasting_indexes=forecast_index)
+
+        # Seasonal Naive
+        df_sn = seasonal_naive_forecasting(train.copy(), last_week, 26, 1, week_to_forecast=len(test.index))
+        # plot_dataframe(df_sn, test, plot_name="Seasonal Naive", forecasting_indexes=forecast_index)
+
+        #  Naive
+        df_n = naive(train.copy(), last_week, week_to_forecast=len(test.index))
+        # plot_dataframe(df_n, test, plot_name="Naive", forecasting_indexes=forecast_index)
+
+        # Drift
+        df_d = driftmethod(train.copy(), last_week, week_to_forecast=len(test.index))
+        plot_dataframe(df_d, test, plot_name="Drift", forecasting_indexes=forecast_index)
+
+        # Seasonal Exp Smoothing
+        df_hw = seasonalExp_smoothing(train.copy(), len(test.index), decompositon=False, box_cox=True)
+        plot_dataframe(df_hw, test, plot_name="Holt-Winters", forecasting_indexes=forecast_index)
+
+        # Simple Exp Smoothing
+        df_ses = smpExpSmoth(train.copy(), len(test.index))
+        plot_dataframe(df_ses, test, plot_name='Simple Exponential Smoothing', forecasting_indexes=forecast_index)
     # TODO
 
 
@@ -16,8 +51,7 @@ def prepare_data(colore):
     dativendita = sommavendite(dativendita)
     dativendita = best20color(dativendita)
     datetoweek(dativendita)
-
-    dativendita = filter_by_color(dativendita, 'nero')
+    dativendita = filter_by_color(dativendita, colore)
     dativendita_colore = weeksdistrubution(dativendita)
     return dativendita_colore
 
